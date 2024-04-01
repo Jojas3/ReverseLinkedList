02d5fe6a4e545f62f53d3038a7cad5c9ac65a0b3
Jojas3 <arandomemail9000@gmail.com>
2024-03-31 20:53:37 -0400

    Linked list reversed!

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,19 @@ public class Main {
         LinkedList<String> myList = new LinkedList<>();
 
 
+        //add 10 new elements to linked list
+        for(int i =0; i<10; i++) {
+            myList.add(i, "E"+i);
+        }
+        //print for validation
+        System.out.println(myList);
+
+        System.out.println(myList.reversed());
+        //heck yeah. built-in function, baby.
+
+
+
+
+
     }
 }
