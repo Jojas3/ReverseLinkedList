9feb1781f50b7ee77084c19f97699c474325be6f
Jojas3 <arandomemail9000@gmail.com>
2024-03-31 20:37:15 -0400

    Initialize the linked list. Need 2 store variables.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is my attempt at reversing a linked list without using the internet or any other external" +
+                "resources. Just good ol' trial and error. Wish me luck.");
+
+        LinkedList<String> myList = new LinkedList<>();
+
+
+    }
+}
